f555b3f29384066fce4e077bd8704549f37b4db0
Carlos Daniel Bayona <carlosdanielbayo@gmail.com>
2018-03-17 23:38:02 -0500

    React Testing

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from "./global/Header.js"
+import Content from "./global/Content.js"
+import Footer from "./global/Footer.js"
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/global/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+      <h1>Soy el Contenido</h1>
+      <p>Holiwi Soy Contenido</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
